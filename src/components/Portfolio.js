437fe3b640dfa94b620764b9a08e884396b65cc3
prajwal.more <prajwal.more@cleartrip.com>
2023-05-18 22:12:37 +0530

    ab

---
+++ src/components/Portfolio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div stydnn edle={{border: `2px solid red`}}>
+            
+        </div>fekl
+    );
+};
+
+export default Portfolio;
